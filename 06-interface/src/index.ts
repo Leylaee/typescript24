470c454f5cc81d5298a80fa50392734f29a249fa
Leylaee <ferhan.96@hotmail.com>
2024-03-19 19:52:30 +0100

    oef

---
+++ 06-interface/src/index.ts
@@ -0,0 +1,64 @@
+interface Dish {
+    name: string;
+    price: number;
+    quantityOrdered: number;
+    discountPercentage?: number;
+    quantityForDiscount?: number;
+}
+
+class MainCourse implements Dish {
+    constructor(
+        public name: string,
+        public price: number,
+        public quantityOrdered: number,
+        public discountPercentage?: number,
+        public quantityForDiscount?: number,
+    ){}
+}
+
+class Dessert implements Dish {
+    constructor(
+        public name: string,
+        public price: number,
+        public quantityOrdered: number,
+        public discountPercentage?: number,
+        public quantityForDiscount?: number,
+    ){}
+}
+
+function calculateTotalPrice(dishes: Dish[]) : number {
+    let totalPrijs = 0;
+
+    for(let i = 0; i < dishes.length; i++){
+        let dish = dishes[i];
+        let totaleprijsVoorDish = dish.price * dish.quantityOrdered;
+        
+        console.log(`${dish.name} - ${dish.price.toFixed(2)}€`);
+
+        if (dish.discountPercentage && dish.quantityForDiscount && dish.quantityOrdered >= dish.quantityForDiscount ){
+           const discountprice = dish.price - (dish.price * dish.discountPercentage /100);
+           totaleprijsVoorDish = discountprice * dish.quantityOrdered;
+           console.log(`Discount of ${dish.discountPercentage}% applied on ${dish.name}`);
+           console.log(`${discountprice.toFixed(2)}€ * ${dish.quantityOrdered} ordered =${(totaleprijsVoorDish).toFixed(2)} `);
+        }else{
+           console.log(`${totaleprijsVoorDish.toFixed(2)}€ * ${dish.quantityOrdered} ordered =${(totaleprijsVoorDish).toFixed(2)} `);
+
+        }  
+        
+        console.log('---');
+
+        totalPrijs += totaleprijsVoorDish;
+    }
+
+    return totalPrijs;
+}
+
+const steak = new MainCourse('Steak',18.99, 4, 10, 1);
+const chickenCurry = new MainCourse('Chicken Curry', 12.99,1);
+const chocolateCake = new Dessert('Chocolate Cake' , 6.99, 4, 15,1);
+const bananaMilkshake = new Dessert('Banana Milkshake', 4.99, 1);
+
+const dishes = [steak, chickenCurry, chocolateCake, bananaMilkshake];
+const totalPrijs = calculateTotalPrice(dishes);
+console.log(`Total price: ${totalPrijs.toFixed(2)}€`);
+
